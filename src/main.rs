d1b3e03ee6e9ef754e5f16c0f89f560f901226e0
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-06-11 19:43:49 +0200

    Using `docx` crate. First attempt to open a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use docx::document::Paragraph;
+use docx::document::Break;
+use docx::DocxFile;
+
 fn main() {
     println!("Hello, world!");
+    let docx_file: DocxFile = DocxFile::from_file("template.docx").unwrap();
+    let docx_file: docx::Docx = docxFile.parse().unwrap();
+
 }
