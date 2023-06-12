9cba0770f0b1d1b989431f5318c1a16ae1b3ab8f
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-06-12 11:38:26 +0200

    Changed to rust_rs library

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use docx::document::Paragraph;
-use docx::document::Break;
-use docx::DocxFile;
+use docx_rs;
+
 
 fn main() {
-    println!("Hello, world!");
-    let docx_file: DocxFile = DocxFile::from_file("template.docx").unwrap();
-    let docx_file: docx::Docx = docxFile.parse().unwrap();
 
+    println!("Hello, world!");
+    // let docx_file: DocxFile = DocxFile::from_file("template.docx").unwrap();
+    // let docx_obj: Docx = docx_file.parse().unwrap();
+    // docx_obj.document.body.content
 }
